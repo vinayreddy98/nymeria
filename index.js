e032ead055ad138ed255c79b64ac19668132ce40
vinayreddy98 <32337904+vinayreddy98@users.noreply.github.com>
2019-01-28 04:40:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,28 @@ app.post('/login', function(req,res){
 });
 
 app.post('/upload', function(req,res) {
+      let sampleFile;
+      let uploadPath;
+
+  if (Object.keys(req.files).length == 0) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
+
+  console.log('req.files >>>', req.files); // eslint-disable-line
+
+  sampleFile = req.files.sampleFile;
+
+  uploadPath = __dirname  + sampleFile.name;
+
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send('File uploaded to ' + uploadPath);
+  });
+});
       res.send('Its working');
 });
 
